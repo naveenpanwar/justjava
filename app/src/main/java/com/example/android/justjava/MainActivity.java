43aa5407979e83f5a4627dcf19fe865d886852ae
Naveen Panwar <naveen.panwar27@gmail.com>
2018-05-18 12:29:51 +0530

    creating a new function for price calculation

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,6 +22,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    public int quantity=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,28 +31,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
-        int quantity = 3;
-        display(quantity);
+        displayQuantity(++quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
-        display(quantity);
+        displayQuantity(--quantity);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
-        display(quantity);
-        displayPrice(quantity * 5 );
+        String priceMessage = "Total :"+ NumberFormat.getCurrencyInstance().format(calculatePrice( quantity, 5 ));
+        displayMessage(priceMessage);
+    }
+
+    public int calculatePrice(int quantity, int pricePerCup) {
+        return quantity * pricePerCup;
+    }
+
+    public void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
